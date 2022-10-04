9158cf5aa9e4d86e3aef077195e64cac4f84e69a
Raunak Singh <raunaksingh@Raunaks-Air.fios-router.home>
2022-10-03 20:22:44 -0400

    added eocvsim and was able to set up sample grayscale cv pipeline

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/computervision/ComputerVision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/computervision/ComputerVision.java
@@ -1,8 +1,9 @@
-package org.firstinspires.ftc.teamcode.util;
+package org.firstinspires.ftc.teamcode.util.computervision;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.util.computervision.pipelines.ComputerVisionPipeline;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
