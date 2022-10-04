d19a8b4e52c8489bdd9dd800220220d8919887b2
gerinome <danielgtravels@gmail.com>
2022-10-04 09:21:15 -0400

    Added auton file, Added opcode, added turntable  motor in config, deleted caurosel motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/DriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/DriveTrain.java
@@ -3,16 +3,36 @@ package org.firstinspires.ftc.teamcode.teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.util.MecanumDrive;
 
 @TeleOp(name="DriveTrain Teleop")
 public class DriveTrain extends LinearOpMode {
+    public static DcMotor fl, fr, bl, br;
+
+    public void initialize(){
+    }
 
     @Override
     public void runOpMode() throws InterruptedException {
+        MecanumDrive drive = new MecanumDrive(hardwareMap);
+        boolean slowMode = false;
 
         waitForStart();
 
         while (opModeIsActive()) {
+            //   slowMode
+            if (gamepad1.dpad_up) slowMode = !slowMode;
+
+            drive.move(gamepad1.left_stick_x * (slowMode ? 0.3 : .7), -gamepad1.left_stick_y * (slowMode ? 0.3 : .7), gamepad1.right_stick_x * (slowMode ? 0.3 : .7));
+
+            // Slides
+
+            // Turntable
+
+            //Claw
+
 
         }
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/CONFIG.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/CONFIG.java
@@ -14,7 +14,7 @@ public class CONFIG {
     public static String LEFTDIST = "left";
     public static String RIGHTDIST = "right";
     public static String ARMMOTOR = "am";
-    public static String CAROUSEL = "carousel";
+    public static String TURNTABLE = "turnt";
     public static String ANGLE = "angle";
     public static double centerOfRobit2BackDistance = 8; //inches
     public static double centerOfRobit2LeftDistance = 8; //inches
