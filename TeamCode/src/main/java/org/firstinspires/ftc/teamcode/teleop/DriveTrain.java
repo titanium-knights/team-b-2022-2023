32327d849a4773d41c2c912a70282ad77c0f7ab7
gerinome <danielgtravels@gmail.com>
2022-10-20 23:05:50 -0400

    Slides: Rewrote, added manual and preset modes. Presets - unchecked Drive train: added preset settings, telemetry, manual

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/DriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/DriveTrain.java
@@ -6,6 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.utilities.Claw;
 import org.firstinspires.ftc.teamcode.utilities.MecanumDrive;
 import org.firstinspires.ftc.teamcode.utilities.Slides;
@@ -15,6 +16,9 @@ public class DriveTrain extends LinearOpMode {
     public static DcMotor fl, fr, bl, br;
 
     public void initialize(){
+        MecanumDrive drive = new MecanumDrive(hardwareMap);
+        Slides slide = new Slides(hardwareMap);
+        Claw claw = new Claw(hardwareMap);
     }
 
     @Override
@@ -22,25 +26,56 @@ public class DriveTrain extends LinearOpMode {
         MecanumDrive drive = new MecanumDrive(hardwareMap);
         Slides slide = new Slides(hardwareMap);
         Claw claw = new Claw(hardwareMap);
+        telemetry.setAutoClear(false);
+
+        //mecanum
         boolean slowMode = false;
+        //slide preset: 0 - orig, 1 - low, 2 - med, 3 - high
+        int mode = 0;
+        int action;
+
 
         waitForStart();
 
         while (opModeIsActive()) {
+
             //   slowMode
             if (gamepad1.dpad_up) slowMode = !slowMode;
 
             drive.move(gamepad1.left_stick_x * (slowMode ? 0.3 : .7), -gamepad1.left_stick_y * (slowMode ? 0.3 : .7), gamepad1.right_stick_x * (slowMode ? 0.3 : .7));
 
             // Slides
+            //Manual control, up
             if (gamepad1.left_trigger > 0) {
-                telemetry.addData("left trigger", "slides up");
-                slide.downFromTop();
+                telemetry.addData("encoder preman", slide.getEncoder());
+                telemetry.addData("left trigger", "slides down");
+                slide.manual(true);
+                telemetry.addData("encoder postman", slide.getEncoder());
+                telemetry.update();
             }
+            //Manual control, down
             else if (gamepad1.right_trigger > 0) {
+                telemetry.addData("encoder preman", slide.getEncoder());
                 telemetry.addData("right trigger", "slides down");
-                slide.topPosition();
+                slide.manual(false);
+                telemetry.addData("encoder postman", slide.getEncoder());
+                telemetry.update();
             }
+
+            //height control
+            action = -1;
+            telemetry.addData("encoder prepreset", slide.getEncoder());
+            if (gamepad1.dpad_down) action = slide.move(0);
+            if (gamepad1.dpad_left) action = slide.move(1);
+            if (gamepad1.dpad_up) action = slide.move(2);
+            if (gamepad1.dpad_right) action = slide.move(3);
+            if (action > -1){
+                telemetry.addData("presetval", action);
+                telemetry.addData("encoder postpreset", slide.getEncoder());
+            }
+
+            if (gamepad1.b) slide.stop();
+
 
             //Claw
             if (gamepad1.left_bumper)
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/Slides.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/Slides.java
@@ -2,52 +2,101 @@ package org.firstinspires.ftc.teamcode.utilities;
 
 import static com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior.BRAKE;
 
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
-
+//NEGATIVE IS UP
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+//ONCE ENCODERS INSTALLED
 public class Slides {
     DcMotor slideMotor;
-    int curpos;
+    //position at initial
+    int initpos;
+    int pos;
+
+    //unknown
+    int maxheight = 0;
+    int midheight = 0;
+    int lowheight = 0;
 
     public Slides(HardwareMap hmap){
+
         this.slideMotor = hmap.dcMotor.get(CONFIG.SLIDE);
-        this.curpos = 0;
-        slideMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        this.initpos = this.pos = 0;
+
         slideMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        slideMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         slideMotor.setZeroPowerBehavior(BRAKE);
+
     }
 
-    public void spin (double dx) { slideMotor.setPower(dx);}
-
-    public void stop() { slideMotor.setPower(0);}
-
-    public int getEncoderPosition() {return slideMotor.getCurrentPosition();}
-
-    //left trigger
-    public void downFromTop() throws InterruptedException {
-        //int pos = slideMotor.getCurrentPosition();
-        //telemetry.addData("startposDown", "value:");
-        slideMotor.setTargetPosition(90);
-        slideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        slideMotor.setPower(0.19);
-        Thread.sleep(30); // 1.7 seconds
+    public void stop(){
+        slideMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         slideMotor.setPower(0);
     }
 
-    //right trigger
-    public void topPosition() throws InterruptedException {
-        //int pos = slideMotor.getCurrentPosition();
-        //telemetry.addData("startposUp", "value:");
-        slideMotor.setTargetPosition(-90);
-        slideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        slideMotor.setPower(-.3);
-        Thread.sleep(30); // 1.7 seconds
-        slideMotor.setPower(0);
-
+    public int getEncoder(){
+        return slideMotor.getCurrentPosition();
     }
 
-    public void setPower(double pwr) { slideMotor.setPower(pwr);}
+    public int getTarget(){
+        return slideMotor.getTargetPosition();
+    }
+
+    public void setTarget(int target){
+        slideMotor.setTargetPosition(target);
+    }
+
+    public void runToPosition(){
+        slideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        slideMotor.setPower(0.3);
+        while (slideMotor.isBusy() && pos > initpos && pos < maxheight) pos = getEncoder();
+        slideMotor.setPower(0);
+        slideMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    // positive target is up, negative is down
+    public int move(int mode){
+        if (slideMotor.isBusy()) return -1;
+
+        int target;
+
+        switch(mode){
+            case 0:
+                target = 0;
+                break;
+            case 1:
+                target = lowheight;
+                break;
+            case 2:
+                target = midheight;
+                break;
+            case 3:
+                target = maxheight;
+                break;
+            default:
+                target = 0;
+        }
+
+        setTarget(target);
+        runToPosition();
+
+        return mode;
+    }
+
+
+
+    // to find min/max, true = up, false = down
+    public void manual(boolean direction){
+        if (slideMotor.isBusy()) return;
+
+        setTarget(pos + (direction ? -1 : 1) * 90);
+        runToPosition();
+    }
+
+
+
+
+
+
 
 }
