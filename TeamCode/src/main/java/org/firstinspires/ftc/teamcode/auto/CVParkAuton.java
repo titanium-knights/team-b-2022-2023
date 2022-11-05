3dca60ae1be8b7dbfe5857123b8cf751643f4c1f
Gabriel Ashkenazi <gabriel.ashkenazi1@gmail.com>
2022-11-05 09:37:22 -0400

    Cleaned up auton code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/CVParkAuton.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode.auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.utilities.MecanumDrive;
+import org.firstinspires.ftc.teamcode.utilities.computervision.ComputerVision;
+import org.firstinspires.ftc.teamcode.utilities.computervision.pipelines.ComputerVisionPipeline;
+
+@Autonomous(name = "CVParkAuton")
+public class CVParkAuton extends LinearOpMode {
+    public void runOpMode() throws InterruptedException {
+        waitForStart();
+
+        MecanumDrive robot = new MecanumDrive(hardwareMap);
+        ComputerVision cv = new ComputerVision();
+        cv.runOpMode();
+
+        String color = ComputerVisionPipeline.color_detected;
+
+        // time-auton written by Gabe
+        // move to spot 1
+
+        if (color.equals("green")) {
+            robot.move(-1,0,0);
+            sleep(800);
+            robot.move(0,0,0);
+            robot.move(0,2,0);
+            sleep(800);
+            robot.move(0,0,0);
+        }
+        // move to spot 2
+        else if (color.equals("yellow")) {
+            robot.move(0,2,0);
+            sleep(800);
+            robot.move(0,0,0);
+        }
+
+        // move to spot 3
+        else if (color.equals("pink")) {
+            robot.move(1,0,0);
+            sleep(800);
+            robot.move(0,0,0);
+            robot.move(0,2,0);
+            sleep(800);
+            robot.move(0,0,0);
+        }
+    }
+}
