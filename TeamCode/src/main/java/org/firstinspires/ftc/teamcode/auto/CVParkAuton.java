fcfa9850eac7e4455780f141b6c73cf612259e9c
Gabriel Ashkenazi <gabriel.ashkenazi1@gmail.com>
2022-11-07 17:46:28 -0500

    Debugging webcam

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/CVParkAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/CVParkAuton.java
@@ -13,37 +13,39 @@ public class CVParkAuton extends LinearOpMode {
         waitForStart();
 
         MecanumDrive robot = new MecanumDrive(hardwareMap);
-        ComputerVision cv = new ComputerVision();
+        ComputerVision cv = new ComputerVision(hardwareMap, telemetry);
         cv.runOpMode();
 
         String color = ComputerVisionPipeline.color_detected;
-
+        sleep(7500);
+        telemetry.addData("color", color);
+        telemetry.update();
         // time-auton written by Gabe
         // move to spot 1
-
-        if (color.equals("green")) {
-            robot.move(-1,0,0);
-            sleep(800);
-            robot.move(0,0,0);
-            robot.move(0,2,0);
-            sleep(800);
-            robot.move(0,0,0);
-        }
-        // move to spot 2
-        else if (color.equals("yellow")) {
-            robot.move(0,2,0);
-            sleep(800);
-            robot.move(0,0,0);
-        }
-
-        // move to spot 3
-        else if (color.equals("pink")) {
-            robot.move(1,0,0);
-            sleep(800);
-            robot.move(0,0,0);
-            robot.move(0,2,0);
-            sleep(800);
-            robot.move(0,0,0);
-        }
+//
+//        if (color.equals("green")) {
+//            robot.move(-1,0,0);
+//            sleep(800);
+//            robot.move(0,0,0);
+//            robot.move(0,2,0);
+//            sleep(800);
+//            robot.move(0,0,0);
+//        }
+//        // move to spot 2
+//        else if (color.equals("yellow")) {
+//            robot.move(0,2,0);
+//            sleep(800);
+//            robot.move(0,0,0);
+//        }
+//
+//        // move to spot 3
+//        else if (color.equals("pink")) {
+//            robot.move(1,0,0);
+//            sleep(800);
+//            robot.move(0,0,0);
+//            robot.move(0,2,0);
+//            sleep(800);
+//            robot.move(0,0,0);
+//        }
     }
 }
