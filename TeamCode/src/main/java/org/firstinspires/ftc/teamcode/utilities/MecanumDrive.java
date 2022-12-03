efd25a6b7c138831d165a144997a02d2432c6db0
gerinome <danielgtravels@gmail.com>
2022-12-03 18:05:19 -0500

    Fixed not going straight

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/MecanumDrive.java
@@ -21,8 +21,6 @@ public class MecanumDrive {
         bl.setDirection(DcMotorSimple.Direction.FORWARD);
         fr.setDirection(DcMotorSimple.Direction.REVERSE);
         br.setDirection(DcMotorSimple.Direction.REVERSE);
-        bl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        bl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         directions.put(fl, new double[]{1, 1});
         directions.put(fr, new double[]{1, -1});
@@ -39,17 +37,7 @@ public class MecanumDrive {
 
     public static HashMap<DcMotor, double[]> directions = new HashMap<>();
 
-    public void forward () throws InterruptedException {
-        fl.setPower(1);
-        br.setPower(-1.7);
-        fr.setPower(-1.5);
-        bl.setPower(0.45);
-        sleep(800);
-        fl.setPower(0);
-        br.setPower(0);
-        fr.setPower(0);
-        bl.setPower(0);
-    }
+
 
     public void move(double x, double y, double turn) {
 
@@ -72,19 +60,5 @@ public class MecanumDrive {
         return a[0] * b[0] + a[1] * b[1];
     }
 
-    public void rotate(double r) {
-        double[] arr = new double[]{fl.getPower() + r, fr.getPower() - r, bl.getPower() + r, br.getPower() - r};
-        double max1 = Math.max(Math.abs(arr[0]), Math.abs(arr[1]));
-        double max2 = Math.max(Math.abs(arr[2]), Math.abs(arr[3]));
-        double max = Math.max(max1, max2);
 
-        for (int i = 0; i < 4; i++) {
-            arr[i] /= max;
-        }
-
-        fl.setPower(arr[0]);
-        fr.setPower(arr[1]);
-        bl.setPower(arr[2]*.5);
-        br.setPower(arr[3]);
-    }
 }
