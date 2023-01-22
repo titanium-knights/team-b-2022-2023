556a3246dfe9f184025254bdb2b297a6a1e79248
umarmustafa200 <iengmustacher@gmail.com>
2023-01-21 23:05:05 -0500

    made left side cone stack auton, constants (compatible with ftc dashboard) need testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/ColorDetec_Stack_Left.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/ColorDetec_Stack_Left.java
@@ -1,59 +1,119 @@
 package org.firstinspires.ftc.teamcode.auto;
 //PUSHED 1/6/23 5:35
 //ALIGN ROBOT WITH INNER BOTTOM CORNER
-//Auton starting on right side of the field
+//Auton starting on left side of the field
 import android.graphics.Color;
-import com.acmerobotics.dashboard.FtcDashboard;
+
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 
 import org.firstinspires.ftc.teamcode.utilities.MecanumDrive;
+import org.firstinspires.ftc.teamcode.utilities.Claw;
+import org.firstinspires.ftc.teamcode.utilities.Slides;
+
 @Config
-@Autonomous(name="ColorDetectionAutonRightSymmetrical")
-public class ColorDetec_RightSymmetrical extends LinearOpMode {
+@Autonomous(name="ColorDetectionStackAutonLeft")
+public class ColorDetec_Stack_Left extends LinearOpMode {
     // Define a variable for our color sensor
     ColorSensor colorSensor;
+    ColorSensor colorSensor2;
     String color;
-    public static int initright = 530;
+
+    public static int junctionright = 490;
+    public static int junctionforward = 270;
+    public static int junctionslowforward = 250;
+    public static int junctionslowback = 250;
+    public static int junctionback = 100;
+    public static int junctionleft = 430;
+
+    public static int initleft = 530;
     public static int initforward = 550;
-    public static int centeringright = 485;
-    public static int returningleft = 485;
+    public static int centeringleft = 515;
+    public static int returningright = 485;
     public static int returningback = 550;
     public static int returninginit = 530;
 
     public static int blueleft = 1700;
-    public static int blueforward = 1100;
+    public static int blueforward = 1200;
 
-    public static int yellowright = 250;
-    public static int yellowforward = 1100;
+    public static int yellowleft = 250;
+    public static int yellowforward = 1500;
 
-    public static int pinkright = 1530;
-    public static int pinkforward = 1200;
+    public static int pinkright = 1200;
+    public static int pinkforward = 1500;
 
     @Override
     public void runOpMode() {
         // Get the color sensor from hardwareMap
-        //This mode uses the left color sensor
-        colorSensor = hardwareMap.get(ColorSensor.class, "colorL");
+        //This mode uses the right color sensor
+        colorSensor = hardwareMap.get(ColorSensor.class, "colorR");
+
 
         // Wait for the Play button to be pressed
         waitForStart();
 
         MecanumDrive robot = new MecanumDrive(hardwareMap);
+        Claw claw = new Claw(hardwareMap);
+        Slides slides = new Slides(hardwareMap);
 
+        claw.close();
+        sleep(800);
+        slides.low();
+        while (slides.isBusy()) {
+            continue;
+        }
+        slides.stop();
 
-        robot.move (0.3, 0,0); //changed from 0.5 to 0.3
-        sleep(initright);
+        robot.move(0.5,0,0);
+        sleep(junctionright);
         robot.move(0,0,0);
         sleep(200);
+        robot.move(0,0.5,0);
+        sleep(junctionforward);
+        robot.move(0, 0.3, 0);
+        sleep(junctionslowforward);
+        robot.move(0,0,0);
+        sleep(200);
+
+        slides.todrop();
+        while (slides.isBusy()) {
+            continue;
+        }
+        slides.stop();
+        claw.open();
+        sleep(200);
+        slides.tozero();
+        while (slides.isBusy()) {
+            continue;
+        }
+        slides.stop();
+
+
+        sleep(200);
+
+        robot.move(0,-0.3,0);
+        sleep(junctionslowback);
+        robot.move(0,-0.5,0);
+        sleep(junctionback);
+        robot.move(0,0,0);
+        sleep(200);
+        robot.move(0.5,0,0);
+        sleep(junctionleft);
+        robot.move(0,0,0);
+        sleep(200);
+
+        robot.move (-0.3, 0,0);
+        sleep(initleft);
+        robot.move(0,0,0);
+        sleep(150);
         robot.move (0, 0.5, 0);
         sleep(initforward);
         robot.move(0, 0, 0);
-        sleep(150);
-        robot.move(0.35, 0, 0); // new addition
-        sleep(centeringright);
+        sleep(100);
+        robot.move(-0.35, 0, 0); // new addition
+        sleep(centeringleft);
         robot.move(0, 0, 0);
         sleep(150);
 
@@ -83,40 +143,41 @@ public class ColorDetec_RightSymmetrical extends LinearOpMode {
             color="blue";
         }
 
-        telemetry.addData("color LEFT SENSOR", color);
+        telemetry.addData("color RIGHT SENSOR", color);
         telemetry.update();
 
         sleep(5000);
 
         // reset
 
-        robot.move(-0.35, 0, 0);
-        sleep(returningleft);
+        robot.move(0.35, 0, 0);
+        sleep(returningright);
         robot.move(0,0,0);
         sleep(150);
         robot.move (0, -0.46, 0);
         sleep(returningback);
         robot.move(0, 0, 0);
         sleep(200);
-        robot.move (-0.3, 0,0);
+        robot.move (0.3, 0,0);
         sleep(returninginit);
         robot.move(0,0,0);
         sleep(200);
 
-        //move to spot 1
+        // Location 1
         if (color.equals("blue")) {
-            robot.move(-0.41,0,0);
+            robot.move(-0.5,0,0);
             sleep(blueleft);
             robot.move(0,0,0);
+            sleep(100);
             robot.move(0,0.5,0);
             sleep(blueforward);
             robot.move(0,0,0);
         }
 
-        // move to spot 2
+        // Location 3
         else if (color.equals("yellow")) {
-            robot.move(0.5,0,0);
-            sleep(yellowright);
+            robot.move(-0.5,0,0);
+            sleep(yellowleft);
             robot.move(0,0,0);
             sleep(100);
             robot.move(0,0.5,0);
@@ -124,11 +185,12 @@ public class ColorDetec_RightSymmetrical extends LinearOpMode {
             robot.move(0,0,0);
         }
 
-        // move to spot 3
+        // Location 3
         else if (color.equals("pink")) {
             robot.move(0.5,0, 0);
-            sleep(pinkright); //changed from 1450
+            sleep(pinkright);
             robot.move(0,0,0);
+            sleep(100);
             robot.move(0,0.5,0);
             sleep(pinkforward);
             robot.move(0,0,0);
